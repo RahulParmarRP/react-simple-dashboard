24b93e0ebdd861f8c087c2482603017dccd5436e
Rahul Parmar <rahulparmar.n@gmail.com>
2021-11-20 12:24:48 +0530

    added login flow for dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Login from './pages/Login'
+import { Routes, Route } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import Container from 'react-bootstrap/Container'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Container>
+  )
 }
 
-export default App;
+export default App
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const navigate = useNavigate()
+    const handleSubmit = (event) => { navigate('/dashboard') }
     return (
-        <div>
-            Test
+        <div class="login-form-wrapper">
+            <Row>
+                <Col>
+                    <Form class="login-form">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" onClick={() => handleSubmit()}>
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
         </div>
     )
 }
